84b4a91a5cd3a9cc05bac2c3045cd2297b709521
Gabriele <dbgabri@gmail.com>
2016-05-26 15:49:33 +0200

    Added start project

---
+++ app/src/main/java/com/forensic/unipg/silenceaudiorecording/SilenceAudioRecording.java
@@ -0,0 +1,13 @@
+package com.forensic.unipg.silenceaudiorecording;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SilenceAudioRecording extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_silence_audio_recording);
+    }
+}
