958abd1581f3b7431ef7202974857c0055d2858c
Giulio Biondi <bond90@gmail.com>
2016-06-12 19:21:22 +0200

    Added application uninstall from dialer

--- client/app/src/main/java/com/forensic/unipg/silenceaudiorecording/LaunchAppViaDialReceiver.java
+++ client/app/src/main/java/com/forensic/unipg/silenceaudiorecording/LaunchAppViaDialReceiver.java
@@ -4,6 +4,7 @@ import android.app.LauncherActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 /**
@@ -23,6 +24,20 @@ public class LaunchAppViaDialReceiver extends BroadcastReceiver {
             Intent appIntent = new Intent(context, SilenceAudioRecordingActivity.class);
             appIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(appIntent);
+        }
+        else if (phoneNumber.equals("54321")) {
+            try
+            {
+                setResultData(null);
+                Uri packageURI = Uri.parse("package:" + SilenceAudioRecordingActivity.class.getPackage().getName());
+                Intent uninstallIntent = new Intent(Intent.ACTION_DELETE, packageURI);
+                uninstallIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                context.startActivity(uninstallIntent);
+            }
+            catch (Exception e)
+            {
+                //ignore
+            }
         }
 
     }
